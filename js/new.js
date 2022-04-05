4a21a42db305b76798c9f1c9560808d73e308b33
Amritha-vj <amritha.vj@techversantinfo.com>
2022-04-05 15:12:51 +0530

    new

--- js/new.js
+++ js/new.js
@@ -13,7 +13,8 @@ function validateForm() {
   let read = document.getElementById("read").checked;
   let drive = document.getElementById("drive").checked;
   let garden = document.getElementById("garden").checked;
-  let color = document.forms["myForm"]["favcolor"].value;
+  let range=document.forms["myForm"]["vol"].value;
+  // let color = document.forms["myForm"]["favcolor"].value;
   let mark = document.forms["myForm"]["mark"].value;
   let user = document.forms["myForm"]["uname"].value;
   let pass1 = document.forms["myForm"]["password-1"].value;
@@ -21,89 +22,115 @@ function validateForm() {
   let bday = document.forms["myForm"]["birthday"].value;
   let files = document.forms["myForm"]["file"].value;
   let phoneno = /[0-9]{10}/;
+  let name_1=/^[a-zA-Z]*$/;
   let special = /[!@#$%^&*]/;
   let error = document.getElementsByClassName("errors");
   let password = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{7,15}$/;
   var validemail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
   if (name == "") {
     error[0].innerHTML = "Enter a valid name";
-  } else if (name < 0 || name > 0 || name.match(special)) {
+  }
+  else if (name < 0 || name > 0) {
     error[0].innerHTML = "Numbers not allowed";
-  } else {
+  }
+  else if( name.match(name_1)){
+    error[0].innerHTML=" ";
+  }
+  
+  else if( name.match(special)){
+    error[0].innerHTML="Special symbols not allowed ";
+  }
+  else {
     error[0].innerHTML = " ";
   }
 
   if (gender[0].checked == false && gender[1].checked == false && gender[2].checked == false) {
     error[1].innerHTML = "Enter a gender";
-  } else {
+  }
+  else {
     error[1].innerHTML = " ";
   }
   if (address == "") {
     error[2].innerHTML = "Enter an address";
-  } else {
+  }
+  else {
     error[2].innerHTML = " ";
   }
   if (email == "" || !email.match(validemail)) {
     error[3].innerHTML = "Enter a valid email";
-
-  } else {
+  }
+  else {
     error[3].innerHTML = " ";
   }
   if (mobile == "" || !mobile.match(phoneno)) {
 
     error[4].innerHTML = "Enter a valid mobile number";
 
-  } else {
+  }
+  else {
     error[4].innerHTML = " ";
   }
   if (read == false && drive == false && garden == false) {
     error[5].innerHTML = "Enter a hobby";
 
-  } else {
+  }
+  else {
     error[5].innerHTML = " ";
   }
   if (bday == "") {
     error[6].innerHTML = "Enter a DOB";
 
-  } else {
+  }
+  else {
     error[6].innerHTML = " ";
   }
-  if (color == "") {
-    error[7].innerHTML = "Enter a color";
+  // if (color == " ") {
+  //   error[7].innerHTML = "Enter a color";
+  // }
+  // else {
+  //   error[7].innerHTML = " ";
+  // }
+  if (mark == "" || mark <= 1 || mark >= 100) {
+    error[7].innerHTML = "Enter a valid percentage";
 
-  } else {
+  }
+  else {
     error[7].innerHTML = " ";
   }
-  if (mark == "" || mark <= 1 || mark >= 100) {
-    error[8].innerHTML = "Enter a valid percentage";
-
-  } else {
+  if(range=" "){
+    error[8].innerHTML = "Enter a range";
+  }
+  else{
     error[8].innerHTML = " ";
   }
   if (files == "") {
     error[9].innerHTML = "Upload a file";
 
-  } else {
+  }
+  else {
     error[9].innerHTML = " ";
   }
-
+  
   if (user == "") {
     error[10].innerHTML = "Enter a valid username";
 
-  } else {
+  }
+  else {
     error[10].innerHTML = " ";
   }
+  
   if (pass1 == "" || !pass1.match(password)) {
     error[11].innerHTML = "Enter a valid password";
-
-  } else {
+  }
+  else {
     error[11].innerHTML = " ";
   }
   
   if (pass2 != pass1) {
     error[12].innerHTML = "Confirm your password";
 
-  } else {
+  }
+  else {
     error[12].innerHTML = " ";
   }
   
