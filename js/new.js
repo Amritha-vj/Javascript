479e4e55cae5f0f5d26e6ebc898f71620b94e3f9
Amritha-vj <amritha.vj@techversantinfo.com>
2022-04-06 18:35:43 +0530

    committed

--- js/new.js
+++ js/new.js
@@ -1,130 +1,186 @@
-let form=document.getElementById("myForm");
+let form = document.getElementById("myForm");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   validateForm();
 });
 
 function validateForm() {
-  let name = document.forms["myForm"]["fname"].value;
+  let name = document.getElementById("fname");
   let gender = document.getElementsByClassName("common");
-  let address = document.forms["myForm"]["address-1"].value;
-  let email = document.forms["myForm"]["email"].value;
-  let mobile = document.forms["myForm"]["mob"].value;
+  let gen = document.getElementById("radio");
+  let address = document.getElementById("address-1");
+  let email = document.getElementById("email");
+  let mobile = document.getElementById("mob");
   let read = document.getElementById("read").checked;
   let drive = document.getElementById("drive").checked;
   let garden = document.getElementById("garden").checked;
-  let range=document.forms["myForm"]["vol"].value;
-  let mark = document.forms["myForm"]["mark"].value;
-  let user = document.forms["myForm"]["uname"].value;
-  let pass1 = document.forms["myForm"]["password-1"].value;
-  let pass2 = document.forms["myForm"]["password-2"].value;
-  let bday = document.forms["myForm"]["birthday"].value;
-  let files = document.forms["myForm"]["file"].value;
+  let hobby = document.getElementById("check");
+  let mark = document.getElementById("mark");
+  let level= document.getElementById("vol");
+  let user = document.getElementById("uname");
+  let pass1 = document.getElementById("password-1");
+  let pass2 = document.getElementById("password-2");
+  let bday = document.getElementById("birthday");
+  let files = document.getElementById("myFile");
   let phoneno = /[0-9]{10}/;
-  let name_1=/^[a-zA-Z]*$/;
+  let name_1 = /^[a-zA-Z]*$/;
   let special = /[!@#$%^&*]/;
-  let error = document.getElementsByClassName("errors");
+  let house= /[A-Za-z0-9'\.\-\s\,]/;
+  // let error = document.getElementsByClassName("errors");
   let password = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{7,15}$/;
   var validemail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-  if (name == "") {
-    error[0].innerHTML = "Enter a valid name";
-  }
-  else if (name < 0 || name > 0) {
-    error[0].innerHTML = "Numbers not allowed";
-  }
-  else if( name.match(name_1)){
-    error[0].innerHTML=" ";
-  }
-  
-  else if( name.match(special)){
-    error[0].innerHTML="Special symbols not allowed ";
-  }
-  else {
-    error[0].innerHTML = " ";
-  }
 
+  if (name.value.trim() == "") 
+  {
+      name.nextElementSibling.innerHTML = "Name required";
+      name.className="error-border";
+  } 
+  else if (!name.value.match(name_1)) 
+  {
+      name.className="error-border";
+      name.nextElementSibling.innerHTML = "Enter a valid name ";
+  } 
+  else 
+  {
+      name.className="green-border";
+      name.nextElementSibling.innerHTML = " ";
+  }
   if (gender[0].checked == false && gender[1].checked == false && gender[2].checked == false) {
-    error[1].innerHTML = "Enter a gender";
+      gen.nextElementSibling.innerHTML = "Gender required";
+  } 
+  else 
+  {
+      gen.nextElementSibling.innerHTML = " ";
   }
-  else {
-    error[1].innerHTML = " ";
-  }
-  if (address == "") {
-    error[2].innerHTML = "Enter an address";
-  }
-  else {
-    error[2].innerHTML = " ";
-  }
-  if (email == "" || !email.match(validemail)) {
-    error[3].innerHTML = "Enter a valid email";
-  }
-  else {
-    error[3].innerHTML = " ";
-  }
-  if (mobile == "" || !mobile.match(phoneno)) {
 
-    error[4].innerHTML = "Enter a valid mobile number";
+  if (address.value.trim() == "") 
+  {
+      address.className="error-border";
+      address.nextElementSibling.innerHTML = "Address required";
+  }
+  else 
+  {
+      address.className="green-border";
+      address.nextElementSibling.innerHTML = " ";
+  }
 
+  if (email.value.trim() == "") 
+  { 
+      email.className="error-border ";
+      email.nextElementSibling.innerHTML = "Email required";
   }
-  else {
-    error[4].innerHTML = " ";
+  else if (email.value!=email.value.match(validemail)) 
+  {
+      email.className="error-border";
+      email.nextElementSibling.innerHTML="Enter a valid Email";
   }
-  if (read == false && drive == false && garden == false) {
-    error[5].innerHTML = "Enter a hobby";
+  else 
+  {
+      email.className="green-border";
+      email.nextElementSibling.innerHTML = " ";
+  }
 
+  if (mobile.value.trim()=="") 
+  {
+      mobile.className="error-border";
+      mobile.nextElementSibling.innerHTML = "Mobile number required";
   }
+  else if (mobile.value!=mobile.value.match(phoneno)) 
+  {
+      mobile.className="error-border";
+      mobile.nextElementSibling.innerHTML="Enter a valid number";
+  }
+  else 
+  {
+      mobile.className="green-border";
+      mobile.nextElementSibling.innerHTML = " ";
+  }
+  if (read == false && drive == false && garden == false) 
+  {
+    hobby.nextElementSibling.innerHTML = "Hobby required";
+  } 
   else {
-    error[5].innerHTML = " ";
+      hobby.nextElementSibling.innerHTML = " ";
   }
-  if (bday == "") {
-    error[6].innerHTML = "Enter a DOB";
+  if (bday.value.trim() == "") 
+  {
+      bday.nextElementSibling.innerHTML = "DOB required";
+  }
+  else 
+  {
+      bday.nextElementSibling.innerHTML = " ";
+  }
 
+  if (mark.value.trim()=="") 
+  {
+      mark.nextElementSibling.innerHTML = "Percentage required";
+      mark.className="error-border";
   }
-  else {
-    error[6].innerHTML = " ";
+  else if( mark.value <= 1 || mark.value >= 100) 
+  {
+      mark.className="error-border";
+      mark.nextElementSibling.innerHTML="Enter a valid percentage";
   }
-  if (mark == "" || mark <= 1 || mark >= 100) {
-    error[7].innerHTML = "Enter a valid percentage";
+  else 
+  {
+      mark.className="green-border";
+      mark.nextElementSibling.innerHTML = " ";
+  }
+  if(level.value.trim() == " ")
+  {
+      level.nextElementSibling.innerHTML="Range required";
+  }
+  else
+  {
+      level.nextElementSibling.innerHTML=" ";
+  }
+  if (files.value.trim() == "") 
+  {
+      files.nextElementSibling.innerHTML = "File required";
+  } 
+  else 
+  {
+      files.nextElementSibling.innerHTML = " ";
+  }
 
+  if (user.value.trim() == " ") 
+  {
+      user.nextElementSibling.innerHTML = "Username required";
+      user.className="error-border";
   }
-  else {
-    error[7].innerHTML = " ";
+  else if(user.value!=user.value.match(validemail))
+  {
+      user.className="error-border";
+      user.nextElementSibling.innerHTML="Enter a valid username";
   }
-  if(range=" "){
-    error[8].innerHTML = "Enter a range";
+  else 
+  {
+      user.className="green-border";
+      user.nextElementSibling.innerHTML = " ";
   }
-  else{
-    error[8].innerHTML = " ";
+  if (pass1.value.trim() == "") 
+  {
+      pass1.className="error-border";
+      pass1.nextElementSibling.innerHTML = "Password required";
   }
-  if (files == "") {
-    error[9].innerHTML = "Upload a file";
-
+  else if(pass1.value!=pass1.value.match(password))
+  {
+      pass1.className="error-border";
+      pass1.nextElementSibling.innerHTML="Enter a valid password";
   }
-  else {
-    error[9].innerHTML = " ";
+  else 
+  {
+      pass1.className="green-border";
+      pass1.nextElementSibling.innerHTML = " ";
   }
-  
-  if (user == "") {
-    error[10].innerHTML = "Enter a valid username";
-
+  if (pass2 != pass1) 
+  {
+      pass2.className="error-border";
+      pass2.nextElementSibling.innerHTML="Confirm your password";
   }
-  else {
-    error[10].innerHTML = " ";
+  else 
+  {
+      pass2.className="green-border";
+      pass2.nextElementSibling.innerHTML="";
   }
-  
-  if (pass1 == "" || !pass1.match(password)) {
-    error[11].innerHTML = "Enter a valid password";
-  }
-  else {
-    error[11].innerHTML = " ";
-  }
-  
-  if (pass2 != pass1) {
-    error[12].innerHTML = "Confirm your password";
-
-  }
-  else {
-    error[12].innerHTML = " ";
-  }
-  
 }
