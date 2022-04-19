0f3495fbbe0ea613cf5353e50e2bb1c5ac2f47f4
Amritha-vj <amritha.vj@techversantinfo.com>
2022-04-19 16:07:58 +0530

    Completed (accordion, tab and modal)

---
+++ Modal/js/modal-js.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+
+    $("#open").click(function () {
+      $(".modalContent").addClass("view");
+    })
+    $("#close").click(function () {
+      $(".modalContent").removeClass("view");
+    });
+  });
+  
--- Tab/js/tab-js.js
+++ Tab/js/tab-js.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
     $('.tab-container:first').show();
-    $('tab-navigation li:first').assClass('active');
-    $('tab-navigation li').click(function(event){
-        index=$(this).index();
-        $('tab-navigation li').removeClass('active');
+    $('.tab-navigation li:first').addClass('active');
+    $('.tab-navigation li').click(function(event){
+        var index=$(this).index();
+        $('.tab-navigation li').removeClass('active');
         $(this).addClass('active');
-        $('tab-container').hide();
-        $('tab-container').eq(index).show();
-    })
-})
+        $('.tab-container').hide();
+        $('.tab-container').eq(index).show();
+    });
+});
